33dfcba7f77840abde9e38b40e4fa5c56a704d67
rishiraj007 <rishiraj@mounttalent.com>
2023-03-06 18:40:24 +0530

    [Prabhu] Add: BST and HAsh Table

---
+++ BST-And-HashTable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BST_And_HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BST-and-Hash Table!");
+        }
+    }
+}
